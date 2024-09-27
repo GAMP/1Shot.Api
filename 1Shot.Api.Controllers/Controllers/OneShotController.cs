1116ee92416ca0a8f25c52f384a0d3166c1aaa1e
Oleg Savelos <olegsavelos@gmail.com>
2024-09-27 12:21:02 +0300

    add some doc.

--- 1Shot.Api.Controllers/Controllers/OneShotController.cs
+++ 1Shot.Api.Controllers/Controllers/OneShotController.cs
@@ -19,20 +19,29 @@ namespace _1Shot.Api.Controllers.Controllers
             _gizmoDbContextProvider = gizmoDbContextProvider;
         }
 
+        /// <summary>
+        /// Gets user by identification string.
+        /// </summary>
+        /// <param name="identification">Identification string.</param>
+        /// <returns>User model, null if no user is found.</returns>
         [HttpGet("getuseridbyidentification")]
-        public User GetUserIdByIdentification([FromQuery] string id)
+        public User? GetUserIdByIdentification([FromQuery] string identification)
         {
-            User result = new();
             using var dbContext = (DbContext)_gizmoDbContextProvider.GetDbNonProxyContext();
             var userMembers = dbContext.Set<Gizmo.DAL.Entities.UserMember>();
-            var member = userMembers.SingleOrDefault(u => u.Identification == id);
-            if (member != null)
-            {
-                result.UserId = member.Id;
-            }
-            return result;
+            var member = userMembers.SingleOrDefault(u => u.Identification == identification);
+            if (member == null)
+                return null;
+
+            return new Models.User() { UserId = member.Id };
         }
 
+        /// <summary>
+        /// Gets if purchase (InvoiceLine) is marked as deleted.
+        /// </summary>
+        /// <param name="invoiceLineId">Invoice line id.</param>
+        /// <param name="cancellationToken">HTTP request cancellation token.</param>
+        /// <returns>True or false, null in case invoice line is not found in database.</returns>
         [HttpGet("isproductdeletedbyinvoicelineid")]
         public async Task<bool?> IsProductDeletedByInvoiceLineIdAsync([FromQuery] int invoiceLineId, CancellationToken cancellationToken = default)
         {
@@ -41,6 +50,12 @@ namespace _1Shot.Api.Controllers.Controllers
             return await invoiceLineTime.Where(product => product.Id == invoiceLineId).Select(product => (bool?)product.IsDeleted).SingleOrDefaultAsync(cancellationToken);
         }
 
+        /// <summary>
+        /// Marks invoice line as deleted.
+        /// </summary>
+        /// <param name="invoiceLineTimeId">Invoice line id.</param>
+        /// <param name="cancellationToken">HTTP request cancellation token.</param>
+        /// <returns>True if marked successfully, in case invoice line is not found in database.</returns>
         [HttpPut("markinvoicelinetimeisdeletedbyid")]
         public async Task<bool> MarkInvoiceLineTimeIsDeletedByIdAsync([FromQuery] int invoiceLineTimeId, CancellationToken cancellationToken = default)
         {
@@ -62,13 +77,19 @@ namespace _1Shot.Api.Controllers.Controllers
             return true;
         }
 
+        /// <summary>
+        /// Gets usable user time purchases (InvoiceLineTime).
+        /// </summary>
+        /// <param name="userId">User id.</param>
+        /// <param name="cancellationToken">HTTP request cancellation token.</param>
+        /// <returns>List of usable time products, empty list in case user has no products or not found in database.</returns>
         [HttpGet("getinvoicelinetimesbyuserid")]
-        public async Task<IEnumerable<object>> GetInvoiceLineTimesByUserIdAsync([FromQuery] int userId, CancellationToken cancellationToken = default)
+        public async Task<IEnumerable<TimeProduct>> GetInvoiceLineTimesByUserIdAsync([FromQuery] int userId, CancellationToken cancellationToken = default)
         {
             using var dbContext = (DbContext)_gizmoDbContextProvider.GetDbNonProxyContext();
             var invoiceLineTime = dbContext.Set<Gizmo.DAL.Entities.InvoiceLineTime>();
             return await invoiceLineTime.Where(il => il.UserId == userId && il.IsDeleted == false && il.IsDepleted == false && il.IsVoided == false)
-                .Select(il => new TimeProduct{ Id = il.Id }).ToListAsync(cancellationToken);
+                .Select(il => new TimeProduct { Id = il.Id }).ToListAsync(cancellationToken);
         }
     }
 }
